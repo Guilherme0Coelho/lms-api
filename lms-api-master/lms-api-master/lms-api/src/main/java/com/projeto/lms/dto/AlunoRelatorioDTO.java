032c5aa3eb4ec28bf1e5a723511c0bc30f1d2784
Guilherme0Coelho <162644557+Guilherme0Coelho@users.noreply.github.com>
2025-12-04 09:02:00 -0300

    Update AlunoRelatorioDTO.java

--- lms-api-master/lms-api-master/lms-api/src/main/java/com/projeto/lms/dto/AlunoRelatorioDTO.java
+++ lms-api-master/lms-api-master/lms-api/src/main/java/com/projeto/lms/dto/AlunoRelatorioDTO.java
@@ -4,13 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// DTO: Objeto para formatar a resposta combinada e agregada.
+
 @Data
 @NoArgsConstructor
 // @AllArgsConstructor
-public class AlunoRelatorioDTO { // <-- CORREÇÃO: Declarar a classe e adicionar "class"
+public class AlunoRelatorioDTO { 
 
-    // 1. Campos (Atributos) devem ser declarados aqui:
+    
     private Long alunoId;
     private String alunoNome;
     private String cursoTitulo;
@@ -18,7 +18,7 @@ public class AlunoRelatorioDTO { // <-- CORREÇÃO: Declarar a classe e adiciona
     private Boolean certificadoEmitido;
     private String statusGeral;
 
-    // 2. Construtor (manual, para resolver o erro) deve estar aqui:
+    
     public AlunoRelatorioDTO(Long alunoId, String alunoNome, String cursoTitulo, Double progressoMatricula, Boolean certificadoEmitido, String statusGeral) {
         this.alunoId = alunoId;
         this.alunoNome = alunoNome;
