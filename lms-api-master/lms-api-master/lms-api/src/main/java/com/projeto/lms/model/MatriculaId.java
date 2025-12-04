88377f6a17ac7566b2baf44d4efee83a7146a607
Guilherme0Coelho <162644557+Guilherme0Coelho@users.noreply.github.com>
2025-12-04 09:11:02 -0300

    Update MatriculaId.java13

--- lms-api-master/lms-api-master/lms-api/src/main/java/com/projeto/lms/model/MatriculaId.java
+++ lms-api-master/lms-api-master/lms-api/src/main/java/com/projeto/lms/model/MatriculaId.java
@@ -12,17 +12,17 @@ public class MatriculaId implements Serializable {
     private Long alunoId;
     private Long cursoId;
 
-    // 1. Construtor Vazio (Obrigatório para o JPA/Testes)
+    
     public MatriculaId() {
     }
 
-    // 2. Construtor Cheio (Para usarmos no teste)
+   
     public MatriculaId(Long alunoId, Long cursoId) {
         this.alunoId = alunoId;
         this.cursoId = cursoId;
     }
 
-    // 3. Getters e Setters manuais (Já que o Lombok falhou)
+   
     public Long getAlunoId() { return alunoId; }
     public void setAlunoId(Long alunoId) { this.alunoId = alunoId; }
 
